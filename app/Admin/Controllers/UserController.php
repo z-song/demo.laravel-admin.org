141c32402114e9ab7621405b2edacb20d6249194
z-song <zosong@126.com>
2018-09-09 21:00:20 +0800

    Update

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -10,6 +10,7 @@ use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
+use Encore\Admin\Widgets\Table;
 
 class UserController extends Controller
 {
